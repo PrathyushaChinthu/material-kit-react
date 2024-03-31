e49490a420511ad4be76489db98e6a1e986ec625
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-31 14:36:00 +0530

    post work  in progress

--- src/sections/post/post-card.jsx
+++ src/sections/post/post-card.jsx
@@ -1,214 +1,196 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import { alpha } from '@mui/material/styles';
-import Grid from '@mui/material/Unstable_Grid2';
-import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+// // import Link from '@mui/material/Link';
+// import Card from '@mui/material/Card';
+// import Stack from '@mui/material/Stack';
+// import Avatar from '@mui/material/Avatar';
+// import { alpha } from '@mui/material/styles';
+// import Grid from '@mui/material/Unstable_Grid2';
+// import Typography from '@mui/material/Typography';
 
-import { fDate } from 'src/utils/format-time';
-import { fShortenNumber } from 'src/utils/format-number';
+// import { fDate } from 'src/utils/format-time';
+// import { fShortenNumber } from 'src/utils/format-number';
 
-import Iconify from 'src/components/iconify';
-import SvgColor from 'src/components/svg-color';
+// import Iconify from 'src/components/iconify';
+// import SvgColor from 'src/components/svg-color';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export default function PostCard({ post, index }) {
-  const { cover, id, userId, title, view, body, comment, share, createdAt } = post;
+// export default function PostCard({ post, index }) {
+//   const { id, userId, title, view, body, comment, share, createdAt } = post;
 
-  const latestPostLarge = index === 0;
+//   const latestPostLarge = index === 0;
 
-  const latestPost = index === 1 || index === 2;
+//   const latestPost = index === 1 || index === 2;
 
-  const renderAvatar = (
-    <Avatar
-      alt={post.id}
-      // src={author.avatarUrl}
-      sx={{
-        zIndex: 9,
-        width: 32,
-        height: 32,
-        position: 'absolute',
-        left: (theme) => theme.spacing(3),
-        bottom: (theme) => theme.spacing(-2),
-        ...((latestPostLarge || latestPost) && {
-          zIndex: 9,
-          top: 24,
-          left: 24,
-          width: 40,
-          height: 40,
-        }),
-      }}
-    />
-  );
+//   const renderAvatar = (
+//     <Avatar
+//       alt={post.id}
+//       // src={author.avatarUrl}
+//       sx={{
+//         zIndex: 9,
+//         width: 32,
+//         height: 32,
+//         position: 'absolute',
+//         left: (theme) => theme.spacing(3),
+//         top: (theme) => theme.spacing(3),
+//         ...(latestPostLarge && {
+//           top: (theme) => theme.spacing(2),
+//           left: (theme) => theme.spacing(2),
+//           width: 40,
+//           height: 40,
+//         }),
+//       }}
+//     />
+//   );
 
-  const renderTitle = (
-    <Link
-      color="inherit"
-      variant="subtitle2"
-      underline="hover"
-      sx={{
-        height: 44,
-        overflow: 'hidden',
-        WebkitLineClamp: 2,
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        ...(latestPostLarge && { typography: 'h5', height: 60 }),
-        ...((latestPostLarge || latestPost) && {
-          color: 'common.white',
-        }),
-      }}
-    >
-      id:{id}
-      <br />
-      userId:{userId}
-      <br />
-      title:{title}
-      <br />
-      body:{body}
-    </Link>
-  );
+//   const renderTitle = (
+//     <Box
+//       sx={{
+//         padding: 2,
+//         backgroundColor: 'primary.main',
+//         borderRadius: 1,
+//         height: 120, // Increase the height here
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'space-around',
+//       }}
+//     >
+//       <Typography variant="subtitle2" color="text.primary">
+//         id: {id}
+//       </Typography>
+//       <Typography variant="subtitle2" color="text.primary">
+//         userId: {userId}
+//       </Typography>
+//       <Typography variant="subtitle2" color="text.primary">
+//         title: {title}
+//       </Typography>
+//       <Typography variant="subtitle2" color="text.primary">
+//         body: {body}
+//       </Typography>
+//     </Box>
+//   );
 
-  const renderInfo = (
-    <Stack
-      direction="row"
-      flexWrap="wrap"
-      spacing={1.5}
-      justifyContent="flex-end"
-      sx={{
-        mt: 3,
-        color: 'text.disabled',
-      }}
-    >
-      {[
-        { number: comment, icon: 'eva:message-circle-fill' },
-        { number: view, icon: 'eva:eye-fill' },
-        { number: share, icon: 'eva:share-fill' },
-      ].map((info, _index) => (
-        <Stack
-          key={_index}
-          direction="row"
-          sx={{
-            ...((latestPostLarge || latestPost) && {
-              opacity: 0.48,
-              color: 'common.white',
-            }),
-          }}
-        >
-          <Iconify width={16} icon={info.icon} sx={{ mr: 0.5 }} />
-          <Typography variant="caption">{fShortenNumber(info.number)}</Typography>
-        </Stack>
-      ))}
-    </Stack>
-  );
+//   const renderInfo = (
+//     <Stack
+//       direction="row"
+//       flexWrap="wrap"
+//       spacing={1.5}
+//       justifyContent="flex-end"
+//       sx={{
+//         mt: 3,
+//         color: 'text.disabled',
+//       }}
+//     >
+//       {[
+//         { number: comment, icon: 'eva:message-circle-fill' },
+//         { number: view, icon: 'eva:eye-fill' },
+//         { number: share, icon: 'eva:share-fill' },
+//       ].map((info, _index) => (
+//         <Stack
+//           key={_index}
+//           direction="row"
+//           sx={{
+//             ...((latestPostLarge || latestPost) && {
+//               opacity: 0.48,
+//               color: 'common.white',
+//             }),
+//           }}
+//         >
+//           <Iconify width={16} icon={info.icon} sx={{ mr: 0.5 }} />
+//           <Typography variant="caption">{fShortenNumber(info.number)}</Typography>
+//         </Stack>
+//       ))}
+//     </Stack>
+//   );
 
-  const renderCover = (
-    <Box
-      component="img"
-      alt={title}
-      src={cover}
-      sx={{
-        top: 0,
-        width: 1,
-        height: 1,
-        objectFit: 'cover',
-        position: 'absolute',
-      }}
-    />
-  );
+//   const renderDate = (
+//     <Typography
+//       variant="caption"
+//       component="div"
+//       sx={{
+//         mb: 2,
+//         color: 'text.disabled',
+//         ...((latestPostLarge || latestPost) && {
+//           opacity: 0.48,
+//           color: 'common.white',
+//         }),
+//       }}
+//     >
+//       {fDate(createdAt)}
+//     </Typography>
+//   );
 
-  const renderDate = (
-    <Typography
-      variant="caption"
-      component="div"
-      sx={{
-        mb: 2,
-        color: 'text.disabled',
-        ...((latestPostLarge || latestPost) && {
-          opacity: 0.48,
-          color: 'common.white',
-        }),
-      }}
-    >
-      {fDate(createdAt)}
-    </Typography>
-  );
+//   const renderShape = (
+//     <SvgColor
+//       color="paper"
+//       src="/assets/icons/shape-avatar.svg"
+//       sx={{
+//         width: 80,
+//         height: 36,
+//         zIndex: 9,
+//         bottom: -15,
+//         position: 'absolute',
+//         color: 'background.paper',
+//         ...((latestPostLarge || latestPost) && { display: 'none' }),
+//       }}
+//     />
+//   );
 
-  const renderShape = (
-    <SvgColor
-      color="paper"
-      src="/assets/icons/shape-avatar.svg"
-      sx={{
-        width: 80,
-        height: 36,
-        zIndex: 9,
-        bottom: -15,
-        position: 'absolute',
-        color: 'background.paper',
-        ...((latestPostLarge || latestPost) && { display: 'none' }),
-      }}
-    />
-  );
+//   return (
+//     <Grid xs={12} sm={latestPostLarge ? 12 : 6} md={latestPostLarge ? 6 : 3}>
+//       <Card sx={{ backgroundColor: 'background.default', borderRadius: 2 }}>
+//         <Box
+//           sx={{
+//             position: 'relative',
+//             pt: 'calc(100% * 3 / 4)',
+//             ...((latestPostLarge || latestPost) && {
+//               pt: 'calc(100% * 4 / 3)',
+//               '&:after': {
+//                 top: 0,
+//                 content: "''",
+//                 width: '100%',
+//                 height: '100%',
+//                 position: 'absolute',
+//                 bgcolor: (theme) => alpha(theme.palette.grey[900], 0.72),
+//               },
+//             }),
+//             ...(latestPostLarge && {
+//               pt: {
+//                 xs: 'calc(100% * 4 / 3)',
+//                 sm: 'calc(100% * 3 / 4.66)',
+//               },
+//             }),
+//           }}
+//         >
+//           {renderShape}
 
-  return (
-    <Grid xs={12} sm={latestPostLarge ? 12 : 6} md={latestPostLarge ? 6 : 3}>
-      <Card>
-        <Box
-          sx={{
-            position: 'relative',
-            pt: 'calc(100% * 3 / 4)',
-            ...((latestPostLarge || latestPost) && {
-              pt: 'calc(100% * 4 / 3)',
-              '&:after': {
-                top: 0,
-                content: "''",
-                width: '100%',
-                height: '100%',
-                position: 'absolute',
-                bgcolor: (theme) => alpha(theme.palette.grey[900], 0.72),
-              },
-            }),
-            ...(latestPostLarge && {
-              pt: {
-                xs: 'calc(100% * 4 / 3)',
-                sm: 'calc(100% * 3 / 4.66)',
-              },
-            }),
-          }}
-        >
-          {renderShape}
+//           {renderAvatar}
+//         </Box>
 
-          {renderAvatar}
+//         <Box
+//           sx={{
+//             p: (theme) => theme.spacing(4, 3, 3, 3),
+//             ...((latestPostLarge || latestPost) && {
+//               width: 1,
+//               bottom: 0,
+//               position: 'absolute',
+//             }),
+//           }}
+//         >
+//           {renderTitle}
 
-          {renderCover}
-        </Box>
+//           {renderDate}
 
-        <Box
-          sx={{
-            p: (theme) => theme.spacing(4, 3, 3, 3),
-            ...((latestPostLarge || latestPost) && {
-              width: 1,
-              bottom: 0,
-              position: 'absolute',
-            }),
-          }}
-        >
-          {renderDate}
+//           {renderInfo}
+//         </Box>
+//       </Card>
+//     </Grid>
+//   );
+// }
 
-          {renderTitle}
-
-          {renderInfo}
-        </Box>
-      </Card>
-    </Grid>
-  );
-}
-
-PostCard.propTypes = {
-  post: PropTypes.object.isRequired,
-  index: PropTypes.number,
-};
+// PostCard.propTypes = {
+//   post: PropTypes.object.isRequired,
+//   index: PropTypes.number,
+// };
