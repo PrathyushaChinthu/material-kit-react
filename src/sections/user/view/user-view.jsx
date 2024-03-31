5588c69e0a747532bcdc1ea9db6f1806d4efff1b
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-31 10:31:04 +0530

    pagenation data is fetching from backend

--- src/sections/user/view/user-view.jsx
+++ src/sections/user/view/user-view.jsx
@@ -1,6 +1,4 @@
-// import { sample } from 'lodash';
-import { useState, useEffect } from 'react';
-// import { faker } from '@faker-js/faker/locale/af_ZA';
+import React, { useState, useEffect } from 'react';
 
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
@@ -17,7 +15,6 @@ import axiosInstance from 'src/helpers/axios';
 import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 
-// import { fetchUsers } from '../../api/api';
 import TableNoData from '../table-no-data';
 import UserTableRow from '../user-table-row';
 import UserTableHead from '../user-table-head';
@@ -25,52 +22,35 @@ import TableEmptyRows from '../table-empty-rows';
 import UserTableToolbar from '../user-table-toolbar';
 import { emptyRows, applyFilter, getComparator } from '../utils';
 
-// ----------------------------------------------------------------------
-
 export default function UserPage() {
   const [page, setPage] = useState(0);
-
   const [order, setOrder] = useState('asc');
-
   const [selected, setSelected] = useState([]);
-
   const [orderBy, setOrderBy] = useState('name');
-
   const [filterName, setFilterName] = useState('');
-
   const [rowsPerPage, setRowsPerPage] = useState(5);
-
   const [users, setUsers] = useState([]);
-
-  // const fakeUsers = Array.from({ length: 100 }, (_, index) => ({
-  //   id: users.id,
-  //   name: users.name,
-  //   username: users.username,
-  //   email: users.email,
-  //   avatarUrl: /assets/images/avatars/avatar_${index + 1}.jpg,
-  //   isVerified: faker.datatype.boolean(),
-  //   status: sample(['active', 'banned']),
-  // }));
-  //
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
+      setLoading(true);
       try {
-        const usersRes = await axiosInstance.get('/users');
-        console.log(usersRes.data);
+        const usersRes = await axiosInstance.get('/users', {
+          params: {
+            page: page + 1,
+            per_page: rowsPerPage,
+          },
+        });
         setUsers(usersRes.data);
       } catch (error) {
-        if (error.response.status === 404) {
-          console.error('Users not found:', error);
-          // Handle 404 error (e.g., display a message to the user)
-        } else {
-          console.error('Error fetching users:', error);
-          // Handle other errors if needed
-        }
+        // Handle errors
+      } finally {
+        setLoading(false);
       }
     };
     fetchUsers();
-  }, []);
+  }, [page, rowsPerPage]);
 
   const handleSort = (event, id) => {
     const isAsc = orderBy === id && order === 'asc';
@@ -116,9 +96,24 @@ export default function UserPage() {
     setRowsPerPage(parseInt(event.target.value, 10));
   };
 
-  const handleFilterByName = (event) => {
+  const handleFilterByName = async (event) => {
     setPage(0);
     setFilterName(event.target.value);
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get('/users', {
+        params: {
+          page: 1,
+          per_page: rowsPerPage,
+          filter: event.target.value,
+        },
+      });
+      setUsers(response.data);
+    } catch (error) {
+      // Handle errors
+    } finally {
+      setLoading(false);
+    }
   };
 
   const dataFiltered = applyFilter({
@@ -129,11 +124,14 @@ export default function UserPage() {
 
   const notFound = !dataFiltered.length && !!filterName;
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Users</Typography>
-
         <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
           New User
         </Button>
@@ -152,7 +150,7 @@ export default function UserPage() {
               <UserTableHead
                 order={order}
                 orderBy={orderBy}
-                rowCount={users.length}
+                rowCount={dataFiltered.length}
                 numSelected={selected.length}
                 onRequestSort={handleSort}
                 onSelectAllClick={handleSelectAllClick}
@@ -183,22 +181,19 @@ export default function UserPage() {
                       handleClick={(event) => handleClick(event, row.name)}
                     />
                   ))}
-
                 <TableEmptyRows
                   height={77}
-                  emptyRows={emptyRows(page, rowsPerPage, users.length)}
+                  emptyRows={emptyRows(page, rowsPerPage, dataFiltered.length)}
                 />
-
                 {notFound && <TableNoData query={filterName} />}
               </TableBody>
             </Table>
           </TableContainer>
         </Scrollbar>
-
         <TablePagination
           page={page}
           component="div"
-          count={users.length}
+          count={dataFiltered.length}
           rowsPerPage={rowsPerPage}
           onPageChange={handleChangePage}
           rowsPerPageOptions={[5, 10, 25]}
@@ -208,6 +203,217 @@ export default function UserPage() {
     </Container>
   );
 }
+
+// // import { sample } from 'lodash';
+// import { useState, useEffect } from 'react';
+// // import { faker } from '@faker-js/faker/locale/af_ZA';
+
+// import Card from '@mui/material/Card';
+// import Stack from '@mui/material/Stack';
+// import Table from '@mui/material/Table';
+// import Button from '@mui/material/Button';
+// import Container from '@mui/material/Container';
+// import TableBody from '@mui/material/TableBody';
+// import Typography from '@mui/material/Typography';
+// import TableContainer from '@mui/material/TableContainer';
+// import TablePagination from '@mui/material/TablePagination';
+
+// import axiosInstance from 'src/helpers/axios';
+
+// import Iconify from 'src/components/iconify';
+// import Scrollbar from 'src/components/scrollbar';
+
+// // import { fetchUsers } from '../../api/api';
+// import TableNoData from '../table-no-data';
+// import UserTableRow from '../user-table-row';
+// import UserTableHead from '../user-table-head';
+// import TableEmptyRows from '../table-empty-rows';
+// import UserTableToolbar from '../user-table-toolbar';
+// import { emptyRows, applyFilter, getComparator } from '../utils';
+
+// // ----------------------------------------------------------------------
+
+// export default function UserPage() {
+//   const [page, setPage] = useState(0);
+
+//   const [order, setOrder] = useState('asc');
+
+//   const [selected, setSelected] = useState([]);
+
+//   const [orderBy, setOrderBy] = useState('name');
+
+//   const [filterName, setFilterName] = useState('');
+
+//   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+//   const [users, setUsers] = useState([]);
+
+//   // const fakeUsers = Array.from({ length: 100 }, (_, index) => ({
+//   //   id: users.id,
+//   //   name: users.name,
+//   //   username: users.username,
+//   //   email: users.email,
+//   //   avatarUrl: /assets/images/avatars/avatar_${index + 1}.jpg,
+//   //   isVerified: faker.datatype.boolean(),
+//   //   status: sample(['active', 'banned']),
+//   // }));
+//   //
+
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       try {
+//         const usersRes = await axiosInstance.get('/users');
+//         console.log(usersRes.data);
+//         setUsers(usersRes.data);
+//       } catch (error) {
+//         if (error.response.status === 404) {
+//           console.error('Users not found:', error);
+//           // Handle 404 error (e.g., display a message to the user)
+//         } else {
+//           console.error('Error fetching users:', error);
+//           // Handle other errors if needed
+//         }
+//       }
+//     };
+//     fetchUsers();
+//   }, []);
+
+//   const handleSort = (event, id) => {
+//     const isAsc = orderBy === id && order === 'asc';
+//     if (id !== '') {
+//       setOrder(isAsc ? 'desc' : 'asc');
+//       setOrderBy(id);
+//     }
+//   };
+
+//   const handleSelectAllClick = (event) => {
+//     if (event.target.checked) {
+//       const newSelecteds = users.map((n) => n.name);
+//       setSelected(newSelecteds);
+//       return;
+//     }
+//     setSelected([]);
+//   };
+
+//   const handleClick = (event, name) => {
+//     const selectedIndex = selected.indexOf(name);
+//     let newSelected = [];
+//     if (selectedIndex === -1) {
+//       newSelected = newSelected.concat(selected, name);
+//     } else if (selectedIndex === 0) {
+//       newSelected = newSelected.concat(selected.slice(1));
+//     } else if (selectedIndex === selected.length - 1) {
+//       newSelected = newSelected.concat(selected.slice(0, -1));
+//     } else if (selectedIndex > 0) {
+//       newSelected = newSelected.concat(
+//         selected.slice(0, selectedIndex),
+//         selected.slice(selectedIndex + 1)
+//       );
+//     }
+//     setSelected(newSelected);
+//   };
+
+//   const handleChangePage = (event, newPage) => {
+//     setPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event) => {
+//     setPage(0);
+//     setRowsPerPage(parseInt(event.target.value, 10));
+//   };
+
+//   const handleFilterByName = (event) => {
+//     setPage(0);
+//     setFilterName(event.target.value);
+//   };
+
+//   const dataFiltered = applyFilter({
+//     inputData: users,
+//     comparator: getComparator(order, orderBy),
+//     filterName,
+//   });
+
+//   const notFound = !dataFiltered.length && !!filterName;
+
+//   return (
+//     <Container>
+//       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+//         <Typography variant="h4">Users</Typography>
+
+//         <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+//           New User
+//         </Button>
+//       </Stack>
+
+//       <Card>
+//         <UserTableToolbar
+//           numSelected={selected.length}
+//           filterName={filterName}
+//           onFilterName={handleFilterByName}
+//         />
+
+//         <Scrollbar>
+//           <TableContainer sx={{ overflow: 'unset' }}>
+//             <Table sx={{ minWidth: 800 }}>
+//               <UserTableHead
+//                 order={order}
+//                 orderBy={orderBy}
+//                 rowCount={users.length}
+//                 numSelected={selected.length}
+//                 onRequestSort={handleSort}
+//                 onSelectAllClick={handleSelectAllClick}
+//                 headLabel={[
+//                   { id: 'name', label: 'Name' },
+//                   { id: 'id', label: 'Id' },
+//                   { id: 'username', label: 'User Name' },
+//                   { id: 'email', label: 'Email' },
+//                   { id: 'isVerified', label: 'Verified', align: 'center' },
+//                   { id: 'status', label: 'Status' },
+//                   { id: '' },
+//                 ]}
+//               />
+//               <TableBody>
+//                 {dataFiltered
+//                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+//                   .map((row) => (
+//                     <UserTableRow
+//                       key={row.id}
+//                       id={row.id}
+//                       name={row.name}
+//                       username={row.username}
+//                       status={row.status}
+//                       email={row.email}
+//                       avatarUrl={row.avatarUrl}
+//                       isVerified={row.isVerified}
+//                       selected={selected.indexOf(row.name) !== -1}
+//                       handleClick={(event) => handleClick(event, row.name)}
+//                     />
+//                   ))}
+
+//                 <TableEmptyRows
+//                   height={77}
+//                   emptyRows={emptyRows(page, rowsPerPage, users.length)}
+//                 />
+
+//                 {notFound && <TableNoData query={filterName} />}
+//               </TableBody>
+//             </Table>
+//           </TableContainer>
+//         </Scrollbar>
+
+//         <TablePagination
+//           page={page}
+//           component="div"
+//           count={users.length}
+//           rowsPerPage={rowsPerPage}
+//           onPageChange={handleChangePage}
+//           rowsPerPageOptions={[5, 10, 25]}
+//           onRowsPerPageChange={handleChangeRowsPerPage}
+//         />
+//       </Card>
+//     </Container>
+//   );
+// }
 
 // import { useState, useEffect } from 'react';
 
